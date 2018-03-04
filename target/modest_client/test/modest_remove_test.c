65a33d5956b7f200bf9ba8e51781656d99529a94
frank <frank@arrakis.com>
2018-03-04 16:30:53 +0100

    Added target code for feature 'remove'

---
+++ target/modest_client/test/modest_remove_test.c
@@ -0,0 +1,14 @@
+
+#include "modest_remove.h"
+int main(int argc, const char * argv[])
+{
+  const char *html = "<div><p>Hello World</p>World</div>";
+  const char *selector = "div p";
+  char* result = modest_select_and_remove(html, selector);
+  // printf("%s\n", result);
+  if(strcmp(result, "<-undef><html><head></head><body><div>World</div></body></html>") != 0){
+    return 1;
+  }
+
+  return 0;
+}
