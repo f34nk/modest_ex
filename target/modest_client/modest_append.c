c95d20dc285fd1742c165ca4fb2aab1172024b27
frank <frank@arrakis.com>
2018-03-06 13:42:41 +0100

    Added method to append nodes; Added 'append' test

--- target/modest_client/modest_append.c
+++ target/modest_client/modest_append.c
@@ -36,65 +36,121 @@ void append_node(myhtml_t *myhtml, myhtml_collection_t *collection, const char* 
 
     for(size_t i = 0; i < collection->length; i++) {
       myhtml_tree_node_t *node = collection->list[i];
+      myhtml_tree_node_t *new_node = get_root_node(myhtml, new_html);
+      myhtml_tree_node_t *last_child = myhtml_node_last_child(node);
+      myhtml_tree_node_t *prev_child = (last_child) ? myhtml_node_prev(last_child) : NULL;
+      // printf("%s, %s, %s, %s\n", (node)?"node":"no node", (new_node)?"new_node":"no new_node", (last_child)?"last_child":"no last_child", (prev_child)?"prev_child":"no prev_child");
 
-      if(node) {
+      if(node && last_child && new_node){
+        myhtml_node_insert_after(last_child, new_node);
+      }
+      else if(node && last_child && new_node == NULL){
+        // check if last_child is a text node
+        myhtml_tag_id_t tag_id = myhtml_node_tag_id(last_child);
+        printf("tag_id %d\n", tag_id);
+        const char *tag_name = myhtml_tag_name_by_id(last_child->tree, tag_id, NULL);
+        printf("tag_name %s\n", tag_name);
+        if(strcmp(tag_name, "-text") == 0){
+          // append new_html as a text to the text of the first child
+          const char *text = myhtml_node_text(last_child, NULL);
+          // char *new_text = strcat(new_html, text);
+          char *new_text = get_concat_string(text, new_html);
 
-        // create a new tree
-        // create new collection from new_html
-        // append root node of new collection to targert node as a child
+          // remove old text node
+          myhtml_node_delete(last_child);
 
-        myhtml_tree_t* new_tree = myhtml_tree_create();
-        myhtml_tree_init(new_tree, myhtml);
-        myhtml_parse(new_tree, MyENCODING_UTF_8, new_html, strlen(new_html));
-  
-        /* create css parser and finder for selectors */
-        mycss_entry_t *css_entry = create_css_parser();
-        modest_finder_t *finder = modest_finder_create_simple();
+          // create new text node from new_text
+          myhtml_tree_node_t* new_text_node = myhtml_node_create(node->tree, MyHTML_TAG__TEXT, MyHTML_NAMESPACE_HTML);
+          mycore_string_t *string = myhtml_node_text_set(new_text_node, new_text, strlen(new_text), MyENCODING_UTF_8);
 
-        const char* selector = "body *";
-        /* parse selectors */
-        mycss_selectors_list_t *selectors_list = prepare_selector(css_entry, selector, strlen(selector));
-
-        /* find nodes by selector */
-        myhtml_collection_t *new_collection = NULL;
-        modest_finder_by_selectors_list(finder, new_tree->node_html, selectors_list, &new_collection);
-
-        size_t j = 0;
-        if(new_collection && new_collection->list && new_collection->length) {
-          if(new_collection->length > 0)
-          {
-            j = 1;
-            myhtml_tree_node_t* new_node = new_collection->list[0]; 
-            if(new_node){
-              myhtml_node_append_child(node, new_node);
-            }
+          if(prev_child){
+            myhtml_node_insert_after(prev_child, new_text_node);
+          }
+          else {
+            myhtml_node_append_child(node, new_text_node);
           }
         }
+        else{
+          const char *new_text = new_html;
+          // create new text node from new_html
+          myhtml_tree_node_t* new_text_node = myhtml_node_create(node->tree, MyHTML_TAG__TEXT, MyHTML_NAMESPACE_HTML);
+          mycore_string_t *string = myhtml_node_text_set(new_text_node, new_text, strlen(new_text), MyENCODING_UTF_8);
 
-        // TODO: How to do proper cleanup here?
-        
-        // /* destroy all */
-        // myhtml_collection_destroy(new_collection);
-
-        // /* destroy selector list */
-        // mycss_selectors_list_destroy(mycss_entry_selectors(css_entry), selectors_list, true);
-
-        // /* destroy Modest Finder */
-        // modest_finder_destroy(finder, true);
-
-        // // destroy MyCSS 
-        // mycss_t *mycss = css_entry->mycss;
-        // mycss_entry_destroy(css_entry, true);
-        // mycss_destroy(mycss, true);
-
-        // /* destroy MyHTML */
-        // // myhtml_t* myhtml = new_tree->myhtml;
-        // myhtml_tree_destroy(new_tree);
-        // // myhtml_destroy(myhtml);
+          if(prev_child){
+            myhtml_node_insert_after(prev_child, new_text_node);
+          }
+          else {
+            myhtml_node_append_child(node, new_text_node);
+          }
+        }
       }
     }
   }
 }
+
+// void append_node(myhtml_t *myhtml, myhtml_collection_t *collection, const char* new_html)
+// {
+//   if(collection && collection->list && collection->length) {
+
+//     for(size_t i = 0; i < collection->length; i++) {
+//       myhtml_tree_node_t *node = collection->list[i];
+
+//       if(node) {
+
+//         // create a new tree
+//         // create new collection from new_html
+//         // append root node of new collection to targert node as a child
+
+//         myhtml_tree_t* new_tree = myhtml_tree_create();
+//         myhtml_tree_init(new_tree, myhtml);
+//         myhtml_parse(new_tree, MyENCODING_UTF_8, new_html, strlen(new_html));
+  
+//         /* create css parser and finder for selectors */
+//         mycss_entry_t *css_entry = create_css_parser();
+//         modest_finder_t *finder = modest_finder_create_simple();
+
+//         const char* selector = "body *";
+//         /* parse selectors */
+//         mycss_selectors_list_t *selectors_list = prepare_selector(css_entry, selector, strlen(selector));
+
+//         /* find nodes by selector */
+//         myhtml_collection_t *new_collection = NULL;
+//         modest_finder_by_selectors_list(finder, new_tree->node_html, selectors_list, &new_collection);
+
+//         if(new_collection && new_collection->list && new_collection->length) {
+//           if(new_collection->length > 0)
+//           {
+//             myhtml_tree_node_t* new_node = new_collection->list[0]; 
+//             if(new_node){
+//               myhtml_node_append_child(node, new_node);
+//             }
+//           }
+//         }
+
+//         // TODO: How to do proper cleanup here?
+        
+//         // /* destroy all */
+//         // myhtml_collection_destroy(new_collection);
+
+//         // /* destroy selector list */
+//         // mycss_selectors_list_destroy(mycss_entry_selectors(css_entry), selectors_list, true);
+
+//         // /* destroy Modest Finder */
+//         // modest_finder_destroy(finder, true);
+
+//         // // destroy MyCSS 
+//         // mycss_t *mycss = css_entry->mycss;
+//         // mycss_entry_destroy(css_entry, true);
+//         // mycss_destroy(mycss, true);
+
+//         // /* destroy MyHTML */
+//         // // myhtml_t* myhtml = new_tree->myhtml;
+//         // myhtml_tree_destroy(new_tree);
+//         // // myhtml_destroy(myhtml);
+//       }
+//     }
+//   }
+// }
 
 /**
  * Append new html as child  at the end of selected node
--- target/modest_client/test/modest_append_test.c
+++ target/modest_client/test/modest_append_test.c
@@ -6,10 +6,42 @@ int main(int argc, const char * argv[])
   const char *selector = "div";
   const char *new_html = "<p>World</p>";
   char* result = modest_select_and_append(html, selector, new_html);
-  // printf("%s\n", result);
+  printf("%s\n", result);
   if(strcmp(result, "<-undef><html><head></head><body><div><p>Hello</p><p>World</p></div></body></html>") != 0){
+    printf("Failed\n");
     return 1;
   }
 
+  html = "<div><p>Hello</p></div>";
+  selector = "div";
+  new_html = "World";
+  result = modest_select_and_append(html, selector, new_html);
+  printf("%s\n", result);
+  if(strcmp(result, "<-undef><html><head></head><body><div><p>Hello</p>World</div></body></html>") != 0){
+    printf("Failed\n");
+    return 1;
+  }
+
+  html = "<div><p>Hello</p></div>";
+  selector = "div p";
+  new_html = " World";
+  result = modest_select_and_append(html, selector, new_html);
+  printf("%s\n", result);
+  if(strcmp(result, "<-undef><html><head></head><body><div><p>Hello World</p></div></body></html>") != 0){
+    printf("Failed\n");
+    return 1;
+  }
+
+  html = "<div><input type=\"radio\"></div>";
+  selector = "div input";
+  new_html = "This will not be appended";
+  result = modest_select_and_append(html, selector, new_html);
+  printf("%s\n", result);
+  if(strcmp(result, "<-undef><html><head></head><body><div><input type=\"radio\"></input></div></body></html>") != 0){
+    printf("Failed\n");
+    return 1;
+  }
+
+  printf("ok\n");
   return 0;
 }
