a0bc13b5082391e3949cd4696ada6c6cee5de9d1
frank <frank@arrakis.com>
2018-03-06 20:07:42 +0100

    Added scope to 'replace'

--- lib/modest_ex/safe/replace.ex
+++ lib/modest_ex/safe/replace.ex
@@ -2,12 +2,8 @@ defmodule ModestEx.Safe.Replace do
    @moduledoc false
   
   def replace(bin, selector, new_bin) do
-    case Nodex.Cnode.call(ModestEx.Safe.Cnode, {:replace, bin <> "\0", selector <> "\0", new_bin <> "\0"}) do
-      {:ok, {:replace, reply}} -> 
-        case reply do
-          <<"<-undef>"::utf8>> <> result -> result
-          _ -> {:error, reply}
-        end
+    case Nodex.Cnode.call(ModestEx.Safe.Cnode, {:replace, bin <> "\0", selector <> "\0", new_bin <> "\0", ModestEx.scope() <> "\0"}) do
+      {:ok, {:replace, result}} -> result
       _ -> {:error, bin}
     end
   end
--- target/modest_client/handle_replace.c
+++ target/modest_client/handle_replace.c
@@ -5,18 +5,20 @@
 
 ETERM *handle_replace(ErlMessage* emsg, ETERM *response){
   if(response == NULL){
-    ETERM *replace_pattern = erl_format("{replace, Html, Selector, NewHtml}");;
+    ETERM *replace_pattern = erl_format("{replace, Html, Selector, NewHtml, Scope}");;
     
     if (erl_match(replace_pattern, emsg->msg))
     {
       ETERM *html = erl_var_content(replace_pattern, "Html");
       ETERM *selector = erl_var_content(replace_pattern, "Selector");
       ETERM *new_html = erl_var_content(replace_pattern, "NewHtml");
+      ETERM *scope = erl_var_content(replace_pattern, "Scope");
       char* html_string = (char*)ERL_BIN_PTR(html);
       char* selector_string = (char*)ERL_BIN_PTR(selector);
       char* new_html_string = (char*)ERL_BIN_PTR(new_html);
+      char* scope_string = (char*)ERL_BIN_PTR(scope);
 
-      const char* result_string = modest_select_and_replace(html_string, selector_string, new_html_string);
+      const char* result_string = modest_select_and_replace(html_string, selector_string, new_html_string, scope_string);
       ETERM* result_bin = erl_mk_binary(result_string, strlen(result_string));
       response = erl_format("{replace, ~w}", result_bin);
 
@@ -24,6 +26,7 @@ ETERM *handle_replace(ErlMessage* emsg, ETERM *response){
       erl_free_term(html);
       erl_free_term(selector);
       erl_free_term(new_html);
+      erl_free_term(scope);
     }
   
     erl_free_term(replace_pattern);
