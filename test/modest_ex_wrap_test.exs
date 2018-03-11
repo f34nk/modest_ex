333ac4bec60b28ab493734f68f90b9639f361e0c
frank <frank@arrakis.com>
2018-03-11 18:19:23 +0100

    Added missing 'wrap' test; Fixed bench

---
+++ test/modest_ex_wrap_test.exs
@@ -0,0 +1,10 @@
+defmodule ModestExWrapTest do
+  use ExUnit.Case
+  doctest ModestEx
+
+  test "can wrap nodes" do
+    result = ModestEx.wrap("<p>Hello</p><p>World</p>", "p", "<div></div>")
+    assert result == "<div><p>Hello</p></div><div><p>World</p></div>"
+  end
+
+end
