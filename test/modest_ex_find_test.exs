14ac9987de4ce2242e06d0b49cd058c5826e15c9
frank <3737950+f34nk@users.noreply.github.com>
2018-04-24 21:41:54 +0200

    Added another test for :contains(test)

--- test/modest_ex_find_test.exs
+++ test/modest_ex_find_test.exs
@@ -2,6 +2,13 @@ defmodule ModestExFindTest do
   use ExUnit.Case
   doctest ModestEx
 
+  test "" do
+    result = File.read!("test/fixtures/lorem_ipsum.html")
+    |> ModestEx.find(":contains(Lorem ipsum)")
+    
+    assert result == ["<h1>Lorem ipsum</h1>", "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"] 
+  end
+
   test "all selectors from file" do
     File.open("test/fixtures/find.csv", [:read], fn file ->
       IO.binstream(file, :line)
