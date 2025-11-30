5ee453d45304d8c223f7b22714a304af88f41f4c
Frank Eickhoff <info@frankeickhoff.de>
2025-11-30 13:08:05 +0100

    Comment out test: find using :contains selector The pseude-class :contains is not implemented yet in Lexbor.

--- test/modest_ex_find_test.exs
+++ test/modest_ex_find_test.exs
@@ -2,12 +2,15 @@ defmodule ModestExFindTest do
   use ExUnit.Case
   doctest ModestEx
 
-  test "" do
-    result = File.read!("test/fixtures/lorem_ipsum.html")
-    |> ModestEx.find(":contains(Lorem ipsum)")
-    
-    assert result == ["<h1>Lorem ipsum</h1>", "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"] 
-  end
+  # TODO: pseudo-class :contains is not yet implemented in Lexbor C library.
+  # Previously, this was exclusively implemented to the Modest library.
+  # See https://github.com/f34nk/Modest
+  # test "find :contains selector" do
+  #   result = File.read!("test/fixtures/lorem_ipsum.html")
+  #   |> ModestEx.find(":contains(Lorem ipsum)")
+
+  #   assert result == ["<h1>Lorem ipsum</h1>", "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"]
+  # end
 
   test "all selectors from file" do
     File.open("test/fixtures/find.csv", [:read], fn file ->
