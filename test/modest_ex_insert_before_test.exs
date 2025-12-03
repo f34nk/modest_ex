174d4eed42aab0de7743bf8d3d768cd855ead82e
Frank Eickhoff <info@frankeickhoff.de>
2025-12-03 11:02:25 +0100

    Implement insert_before/3 with lexbor_erl

--- test/modest_ex_insert_before_test.exs
+++ test/modest_ex_insert_before_test.exs
@@ -2,7 +2,6 @@ defmodule ModestExInsertBeforeTest do
   use ExUnit.Case
   # doctest ModestEx
 
-  @tag :skip
   test "insert new html before selected node" do
     result = ModestEx.insert_before("<div><p>World</p></div>", "div p", "<p>Hello</p>")
     assert result == "<div><p>Hello</p><p>World</p></div>"
